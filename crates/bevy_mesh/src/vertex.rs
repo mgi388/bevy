16efb1ad9b3fb2e3efb24407cb709be19970aa7d
mgi388 <>
2024-12-19 21:50:45 +1100

    Add specialize_vertex_attributes

--- crates/bevy_mesh/src/vertex.rs
+++ crates/bevy_mesh/src/vertex.rs
@@ -116,6 +116,7 @@ pub struct MissingVertexAttributeError {
     name: &'static str,
 }
 
+#[derive(Debug)]
 pub struct VertexAttributeDescriptor {
     pub shader_location: u32,
     pub id: MeshVertexAttributeId,
